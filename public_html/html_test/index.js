6f4b19e359e3a2825c056d3174ffe10b3028d397
Makoto NAKASHIMA <makoto.nksm+git@gmail.com>
2009-12-20 19:04:45 +0900

    ???

--- public_html/html_test/index.js
+++ public_html/html_test/index.js
@@ -5,7 +5,8 @@ $(document).ready(
     google.setOnLoadCallback(
       function() {
         var map = new google.maps.Map2(document.getElementById("map"));
-        map.setCenter(new google.maps.LatLng(35.60709019396141,139.6853256225586),19);
+        map.setCenter(
+          new google.maps.LatLng(35.60709019396141,139.6853256225586),16);
         map.addControl(new GLargeMapControl());
         map.addControl(new GMapTypeControl());
         map.addControl(new GOverviewMapControl());
@@ -13,8 +14,14 @@ $(document).ready(
 
         var blueIcon = new GIcon(G_DEFAULT_ICON);
         blueIcon.image = "http://www.openspc2.org/Google/Maps/marker/color/0x6af.png";
-        map.addOverlay(new GMarker(new GPoint(139.68549862504005, 35.60725592797462), {icon: blueIcon}));
-        map.addOverlay(new GMarker(new GPoint(139.68528673052788, 35.607098916812774), {}));
+        map.addOverlay(
+          new GMarker(
+            new GPoint(139.68549862504005, 35.60725592797462),
+            {icon: blueIcon}));
+        map.addOverlay(
+          new GMarker(
+            new GPoint(139.68528673052788, 35.607098916812774),
+            {}));
 
 
         // IEでのメモリリークを防止
