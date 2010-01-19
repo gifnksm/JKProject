5795b0f51d871389df535d7c7d4a1b6e5311ba1f
Makoto NAKASHIMA <makoto.nksm+git@gmail.com>
2010-01-20 06:12:40 +0900

    各店舗の総合評価のやり方を変更 (平均点から最悪値へ)

--- public_html/html_test2/response.php
+++ public_html/html_test2/response.php
@@ -20,28 +20,32 @@ $building_data = json_decode($json_str, true);
 
 /* スコアリング関数 */
 function get_score_name($score) {
-  if ($score < 4)
-    return 'blue';
-  if ($score < 7)
+  if ($score <= 5)
+    return 'red';
+  if ($score <= 8)
     return 'yellow';
-  return 'red';
+  return 'blue';
 }
 
 /* score および scoreDetail を設定 */
 foreach ($building_data as &$building) { /* &をつけてループ内で代入可能に */
   $detail = array();
-  $score_sum = 0;
+  $score_base = rand(5, 10);
+  $score_arr = array();
   foreach ($cat_ids as $id) {
     /* カテゴリのスコアを設定 */
-    $score = rand(0, 10);
+    $score = rand(-5, 5) + $score_base; /* 値が偏りやすくなるようにbaseでバイアス */
+    if ($score < 0) $score = 0;
+    elseif($score > 10) $score = 10;
+
     $detail[$id] = array('color' => get_score_name($score),
                          'value' => $score);
-    $score_sum += $score;
+    $score_arr[] = $score;
   }
-  /* 各カテゴリのスコアの平均値で施設のスコアを決定 */
-  $avg = $score_sum / count($cat_ids);
-  $building['score'] = array('color' =>get_score_name($avg),
-                             'value' => $avg,
+  /* 各カテゴリスコアの最悪値 (最小値) で施設のスコアを決定 */
+  $score = min($score_arr);
+  $building['score'] = array('color' => get_score_name($score),
+                             'value' => $score,
                              'detail' => $detail);
 }
 
