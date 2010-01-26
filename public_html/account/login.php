e9a656525a942eda4f8855f85c9798462b7aea89
hiroki <hiroki@s211-222.pubnet.titech.ac.jp>
2010-01-27 00:13:57 +0900

    account/*.php update

--- public_html/account/login.php
+++ public_html/account/login.php
@@ -10,17 +10,19 @@
 <HTML>
 <HEAD><TITLE>ログイン</TITLE></HEAD>
 <BODY>
-<table><tr><td style="width: 12em;">
-               <A Href="/../html_test2/index.html" Target="_blank">   
-	       <h1><img src="/resource/image/logo_small.png" Border="0" with="200" height="50"
-	       		alt="バリアフリーマップ検索" /></h1>
-		</td><td style="text-align: right;">
-</td>
-</tr>
+<table>
+      <td style="width: 12em;">
+         <A Href="/../html_test2/index.html" Target="_blank">   
+	   <h1><img src="/resource/image/logo_small.png" Border="0" with="200" height="50" alt="バリアフリーマップ検索" />
+	   </h1>
+      </td>
 </table>
 <DIV align="center">
-<h1><img src="./image/login_3.png"
-                        alt="バリアフリーマップ検索" /></h1>
+      <td align="center">
+          <h1><img src="./image/login_3.png" alt="バリアフリーマップ検索" /></h1>
+      </td>
+   </td>
+</table>
     <FORM method="post" action="login2.php">
         ユーザ名: <INPUT type="text" name="username"><BR>
         パスワード: <INPUT type="password" name="password"><BR>
@@ -32,17 +34,17 @@
 <p><a href="account.php">アカウント新規登録</a></p>
 <p><a href="/../html_test2/index.html">ホーム</a></p>
 <A Href="javascript:history.go(-1)">1つ前に戻る</A> 
+</DIV>
 
-</DIV>
-<DIV align="right">
-<table><tr><td style="width: 12em;">
-               <h1><img src="./image/car_1.png" with="300" height="225"
-                        alt="車いす" /></h1>
-                </td><td style="text-align: right;">
-</DIV>
+<table>
+  <tr>
+    <td align="right">
+      <h1><img src="./image/car_1.png" with="300" height="225" alt="車いす" /></h1>
+    </td>
+ </tr>
+</table>
 
 <SCRIPT type="text/JavaScript">
-<!--
 var pic = new Array();
 pic[0] = new Image();
 pic[1] = new Image();
@@ -51,13 +53,7 @@ pic[1].src = "./image/login_1.png";
 function setRollOverImg(index, obj){
 obj.src = pic[index].src;
 }
-//-->
 </SCRIPT>
 <IMG src="./image/login_1.png" onMouseOver="setRollOverImg(0, this)" onMouseOut="setRollOverImg(1, this)">
-
-
-
-
-
 </BODY>
 </HTML>
